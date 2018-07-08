1d3ef63e9f883b55e7cd6ab28fe031d531575416
sebastian vega <seba.kc@gmail.com>
2018-07-08 13:55:59 -0400

    centros de acopio

--- src/views/Centers.vue
+++ src/views/Centers.vue
@@ -6,7 +6,49 @@
         align-center
       >
         <v-flex text-xs-center>
-          <h1>how to</h1>
+
+          <template>
+            <v-container fluid grid-list-md>
+              <v-data-iterator
+                :items="items"
+                :rows-per-page-items="rowsPerPageItems"
+                :pagination.sync="pagination"
+                content-tag="v-layout"
+                row
+                wrap
+              >
+                <v-flex
+                  slot="item"
+                  slot-scope="props"
+                  xs12
+                  sm6
+                  md4
+                  lg3
+                >
+                  <v-card>
+                    <v-card-title><h4>{{ props.item.name }}</h4></v-card-title>
+                    <v-divider></v-divider>
+                    <v-list dense>
+                      <v-list-tile>
+                        <v-list-tile-content>Ubicacion:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.calories }}</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-content>Numero de contacto:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.fat }}</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-content>Tiene espacio:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.carbs }}</v-list-tile-content>
+                      </v-list-tile>
+                      
+                    </v-list>
+                  </v-card>
+                </v-flex>
+              </v-data-iterator>
+            </v-container>
+          </template>
+
         </v-flex>
       </v-layout>
     </v-container>
@@ -17,10 +59,33 @@
   export default {
     name: 'Centers',
     data: () => ({
-
-    }),
-    props: {
-      source: String
-    }
+      rowsPerPageItems: [4, 8, 12],
+      pagination: {
+        rowsPerPage: 4
+      },
+      items: [
+        {
+          name: 'Centro las cerro colorado',
+          calories: 'Cerro colorado 5870, las Condes',
+          fat: '+56 9 65654654',
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%'
+        },
+         {
+          value: false,
+          name: 'Centro las cerro colorado',
+          calories: 'Cerro colorado 5870, las Condes',
+          fat: '+56 9 65654654',
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%'
+        },
+      ]
+    })
   }
 </script>
--- src/views/HowTo.vue
+++ src/views/HowTo.vue
@@ -6,7 +6,64 @@
         align-center
       >
         <v-flex text-xs-center>
-          <h1>how to</h1>
+
+          <template>
+            <v-container fluid grid-list-md>
+              <v-data-iterator
+                :items="items"
+                :rows-per-page-items="rowsPerPageItems"
+                :pagination.sync="pagination"
+                content-tag="v-layout"
+                row
+                wrap
+              >
+                <v-flex
+                  slot="item"
+                  slot-scope="props"
+                  xs12
+                  sm6
+                  md4
+                  lg3
+                >
+                  <v-card>
+                    <v-card-title><h4>{{ props.item.name }}</h4></v-card-title>
+                    <v-divider></v-divider>
+                    <v-list dense>
+                      <v-list-tile>
+                        <v-list-tile-content>Calories:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.calories }}</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-content>Fat:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.fat }}</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-content>Carbs:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.carbs }}</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-content>Protein:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.protein }}</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-content>Sodium:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.sodium }}</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-content>Calcium:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.calcium }}</v-list-tile-content>
+                      </v-list-tile>
+                      <v-list-tile>
+                        <v-list-tile-content>Iron:</v-list-tile-content>
+                        <v-list-tile-content class="align-end">{{ props.item.iron }}</v-list-tile-content>
+                      </v-list-tile>
+                    </v-list>
+                  </v-card>
+                </v-flex>
+              </v-data-iterator>
+            </v-container>
+          </template>
+
         </v-flex>
       </v-layout>
     </v-container>
@@ -15,12 +72,25 @@
 
 <script>
   export default {
-    name: 'HowTo',
     data: () => ({
+      rowsPerPageItems: [4, 8, 12],
+      pagination: {
+        rowsPerPage: 4
+      },
+      items: [
+        {
+          value: false,
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%'
+        },
 
-    }),
-    props: {
-      source: String
-    }
+      ]
+    })
   }
 </script>
